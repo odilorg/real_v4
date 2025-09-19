1227a664a3e723241364cb0f7fefcda6b06ba4af
odil <odilorg@gmail.com>
2025-09-19 17:28:21 +0500

    first commit

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Schemas\Schema;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DateTimePicker;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email()
+                    ->required(),
+                DateTimePicker::make('email_verified_at'),
+                TextInput::make('password')
+                    ->password()
+                    ->required(),
+                    // Using Select Component
+Select::make('roles')
+    ->relationship('roles', 'name')
+    ->multiple()
+    ->preload()
+    ->searchable(),
+            ]);
+    }
+}
